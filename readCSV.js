c20add7f201cf142d1337d1381686c3204cfbff3
MendioX <82616448+MendioX@users.noreply.github.com>
2023-01-20 16:52:33 -0300

    fix de porcentajes y valor factura

--- readCSV.js
+++ readCSV.js
@@ -1,5 +1,69 @@
 let sectorCount = [];
 
+function parseToMinits(time) {
+   let minits = 0;
+   for (let i = 0; i < time.length; i++) {
+        
+      switch (i) {
+         //posicion 0 = horas - del vector
+         case 0:
+            
+            minits += time[i]*60;
+            break;
+            //posicion 1 = min - del vector
+         case 1:
+            minits += time[i];
+            break;
+            //posicion 2 = seg - del vector
+         case 2:
+           minits += Math.round(time[i]/60);
+            break;
+      }
+   }
+
+   return minits;
+}
+
+function sumarTimeNac(time,arrAux2,NameregJson){
+
+   arrAux2.map(element => {
+
+   if (element.nameSector == NameregJson) {
+      
+         
+         element["timeCallNac"] += parseToMinits(time);          
+         element["count"] += 1;
+   }
+      
+   });
+
+}
+
+function sumarTimeInt(time,arrAux2,NameregJson){
+
+   arrAux2.map(element => {
+   
+      if (element.nameSector == NameregJson) {
+         element["timeCallInt"] += parseToMinits(time);          
+         element["count"] += 1;}
+         
+      });
+   
+   }
+   
+
+
+function checkNameReg(NameregJson,arrAux2) {
+   let flag = false;
+   arrAux2.map(element => {
+   if (element.nameSector == NameregJson) {
+      flag = true;
+      return flag;
+   }
+    
+});
+return flag; 
+}
 
 function checkArrayEmpty(time){
 
@@ -11,20 +75,29 @@ function checkArrayEmpty(time){
 
 }
 
-function filtroNumero(registro,sectorCount){
+function filtroNumero(registro,arrAux2){
    const fieldTime='talk time'; 
    const fieldTel='number';
-          
+   let objTemp = {
+      nameSector : "",
+
+   }        
    
-   if (((/out|SALIENTES/gi).test(registro.queue)) && sectorCount.hasOwnProperty(registro.queue) == false)
+   // if (((/out|SALIENTES/gi).test(registro.queue)) && sectorCount.hasOwnProperty(registro.queue) == false)
+   if (((/out|SALIENTES/gi).test(registro.queue)) && checkNameReg(registro.queue,arrAux2) == false)
    {
        if((/^00/).test(registro.number))
        {
 
           let time = registro[fieldTime].split(":").map(Number);//array 3 pos [H,Min,Seg]
-         if (checkArrayEmpty(time))
+       if (checkArrayEmpty(time))
          {
-            sectorCount[registro.queue] = {count:1, timeCallNac: [0,0,0],timeCallInt :time};  
+           // sectorCount[registro.queue.toString()] = {count:1, timeCallNac: [0,0,0],timeCallInt :time};  
+            objTemp["nameSector"]= registro.queue;
+            objTemp["count"] = 1;
+            objTemp["timeCallNac"] = 0;
+            objTemp["timeCallInt"] = parseToMinits(time);
+            arrAux2.push(objTemp);
          }
           
 
@@ -32,12 +105,18 @@ function filtroNumero(registro,sectorCount){
          let time = registro[fieldTime].split(":").map(Number);//array 3 pos [H,Min,Seg]
          if (checkArrayEmpty(time))
          {
-            sectorCount[registro.queue] = {count:1, timeCallNac: time,timeCallInt :[0,0,0]};
+           // sectorCount[registro.queue.toString()] = {count:1, timeCallNac: time,timeCallInt :[0,0,0]};
+            objTemp["nameSector"]= registro.queue; 
+            objTemp["count"] = 1;
+            objTemp["timeCallNac"] = parseToMinits(time);
+            objTemp["timeCallInt"] = 0;
+            arrAux2.push(objTemp);
+            //console.log(sectorCount[0].count)
          }
        }
     
    }
-      if(sectorCount.hasOwnProperty(registro.queue) == true) 
+      if(checkNameReg(registro.queue,arrAux2) == true) 
       {
          if ((/^00/).test(registro.number)) {
          let time = registro[fieldTime].split(":").map(Number);
@@ -45,12 +124,7 @@ function filtroNumero(registro,sectorCount){
          if (checkArrayEmpty(time))
          {
 
-            for (let i = 0; i < time.length; i++) 
-             {
-            sectorCount[registro.queue].timeCallInt[i] += time[i]; 
-             }
-
-            sectorCount[registro.queue].count ++;
+           sumarTimeInt(time,arrAux2,registro.queue)
 
          }
          
@@ -59,20 +133,11 @@ function filtroNumero(registro,sectorCount){
          let time = registro[fieldTime].split(":").map(Number);
          if (checkArrayEmpty(time))
          {
-
-            for (let i = 0; i < time.length; i++) 
-            {
-               sectorCount[registro.queue].timeCallNac[i] += time[i]; 
-            }
-      
-            sectorCount[registro.queue].count ++;
-         }
-
-
+            sumarTimeNac(time,arrAux2,registro.queue)
          }
            
       }
-   
+      }
 }
 
 
@@ -80,13 +145,14 @@ function cargarJSON(myJson){
     // fetch(myJson)
     // .then(respuesta => respuesta.json())//indicamos formato de obtencion de info
     // .then(registro =>{
-         myJson.forEach(registro => {
+         myJson.map(registro => {
          
             filtroNumero(registro,sectorCount);
       
          });
-        
+        console.log(sectorCount[0])
         //})//mostamos info
+        calculoPorcentaje(sectorCount)
 }
 
 
@@ -165,5 +231,61 @@ function abrirArchivo(evento){
 window.addEventListener('load',()=>{
     document.getElementById('uploadFile').addEventListener('change', abrirArchivo);
 })
+
+
+function calcPorcentDpto(depto, minTotales){
+   let calc =(depto.timeCallInt + depto.timeCallNac)*100/minTotales;
+   return (calc.toFixed(2));
+
+}
+
+function calcPorcentMonto(monto,auxPorc){
+
+   return ((auxPorc*monto/100).toFixed(2))
+}
+
+function calculoPorcentaje(arrLlamadas){
+
+   let totalNac = 0;
+   let totalInt = 0;
+   let temp = "";
+   
+   let minTotalTemp ="";
+   let monto = parseInt(document.getElementById("valorFactura").value); 
+   console.log((monto) );
+   
+   arrLlamadas.map(depto =>{
+
+      totalNac += depto.timeCallNac;
+      totalInt += depto.timeCallInt;
+     
+   })
+
+   let minTotales = totalNac + totalInt;
+   console.log(minTotales)
+ arrLlamadas.forEach(dpto => {
+   let auxPorc = calcPorcentDpto(dpto, minTotales);
+   console.log(auxPorc)
+   temp += "<tr>"
+   temp += "<td>"+dpto.nameSector+"</td>"
+   temp += "<td>"+dpto.timeCallNac+"</td>"
+   temp += "<td>"+dpto.timeCallInt+"</td>"
+   temp += "<td>"+dpto.count+"</td>"
+   temp += "<td> % "+auxPorc+"</td>"
+   temp += "<td> $ "+calcPorcentMonto(monto,auxPorc)+"</td>"
+   temp += "</tr>"
+
+ });
+
+ minTotalTemp = "<label id=\"total\"> Minutos totales: "+(totalInt+totalNac)+"</label>";
+ document.getElementById("newLabel").innerHTML = minTotalTemp;
+ document.getElementById("bodytable").innerHTML = temp;
+ 
+}
+
+
+
+document.querySelector('#calcular').addEventListener('click',calculoPorcentaje(sectorCount))
+//console.log((sectorCount.names()));
 
 console.log(sectorCount);
